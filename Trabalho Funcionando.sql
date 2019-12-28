fc9f94f37f771731e882b2d962d55e70fc52acbe
Kleber Benites <36742090+KekeBenites@users.noreply.github.com>
2019-12-28 11:35:15 -0300

    Atualização
    Trabalho finalizado

--- Trabalho Funcionando.sql
+++ Trabalho Funcionando.sql
@@ -1,13 +1,5 @@
 create database sindico_online_db;
-
 use sindico_online_db;
-
-drop table morador;
-drop table apto;
-drop table bloco;
-drop table predio;
-drop table sindico;
-
 create table predio (
 idPredio int auto_increment primary key,
 nomePredio varchar(50)
@@ -27,7 +19,6 @@ numeroApto int not null,
 idBloco int not null,
 foreign key (idBloco) references bloco(idBloco)
 );
-
 
 create table morador(
 idMorador int auto_increment primary key,
@@ -74,6 +65,16 @@ idPredio int not null,
 foreign key(idPredio) references predio(idPredio));
 
 
+create table boleto(
+idBoleto int auto_increment primary key not null,
+valor decimal(10,2),
+vencimento date,
+posicao boolean,
+idMorador int,
+foreign key (idMorador) references morador(idMorador)
+);
+
+
 create table multa(
 idMulta int auto_increment primary key not null,
 descricaoMulta varchar(50) not null,
@@ -81,3 +82,57 @@ valorMulta decimal(10,2),
 idMorador int not null,
 nomeSindico varchar(50) not null,
 foreign key (idMorador) references morador(idMorador));
+
+
+select avisos.idAviso, avisos.descricaoAviso, predio.nomePredio  from avisos inner join predio on predio.idPredio = avisos.idPredio;
+
+SELECT * FROM apto inner join morador where idBloco = 9 and morador.idApto; 
+
+select * from apto where idBloco = 9 ;
+select * from morador;
+
+
+select * from predio;
+select * from bloco;
+select * from apto;
+select * from morador;
+select * from reclamacao;
+
+SELECT idApto FROM apto where numeroApto = 1001  and idBloco = 2;
+
+SELECT idBloco FROM bloco where nomeBloco = "C" and idPredio = 2;
+SELECT * FROM bloco where idPredio = 6;
+select idApto from morador;
+
+SELECT idApto FROM apto where idBloco = 6 and not exists (select idApto from morador where idApto = apto.idApto); 
+
+insert into reclamacao(idMorador,descricaoReclamacao) values (1,"Barulho após o horario");
+insert into reclamacao(idMorador,descricaoReclamacao) values (1,"Bicicleta");
+insert into reclamacao(idMorador,descricaoReclamacao) values (1,"Bebida");
+insert into reclamacao(idMorador,descricaoReclamacao) values (2,"Xixi de Cachorro");
+
+-- Morador
+select * from reclamacao where idMorador = 1;
+
+select idMorador from morador where nomeMorador = 'kleber';
+
+select  * from bloco inner join predio on bloco.idPredio = predio.idPredio;
+select * from morador inner join bloco on bloco.idBloco = morador.idBloco;
+select * from apto inner join bloco on apto.idBloco = bloco.idBloco;
+
+select predio.nomePredio, bloco.nomeBloco, bloco.valorBloco from bloco inner join predio on bloco.idPredio = predio.idPredio;
+select bloco.idBloco,bloco.nomeBloco,bloco.valorBloco, predio.nomePredio from bloco inner join predio on bloco.idPredio = predio.idPredio;
+select apto.idApto, predio.nomePredio, bloco.nomeBloco, apto.numeroApto  from bloco inner join apto   on  bloco.idBloco = apto.idBloco  inner join predio on bloco.idPredio = predio.idPredio;
+                                                                                   
+  select  predio.idPredio from predio where nomePredio = "UNG";                                                                                
+                                                                                   
+select morador.idMorador, morador.nomeMorador, morador.cpfMorador, apto.numeroApto, bloco.nomeBloco, predio.nomePredio, morador.loginMorador, morador.senhaMorador from morador inner join apto on morador.idApto = apto.idApto inner join bloco on apto.idBloco = bloco.idBloco inner join predio on bloco.idPredio = predio.idPredio;
+
+SELECT numeroApto FROM apto where idBloco = 1;
+
+select * from predio;
+
+select predio.idPredio from morador inner join apto on morador.idApto = apto.idApto inner join bloco on apto.idBloco = bloco.idBloco inner join predio on bloco.idPredio = predio.idPredio where nomePredio  = "FMU";
+
+select nomePredio from predio inpredioner join bloco on bloco.idPredio = predio.idPredio inner join apto on apto.idBloco = bloco.idBloco inner join morador on apto.idApto = morador.idApto where idMorador = 2;
+
--- readme.txt
+++ readme.txt
@@ -1,12 +1,7 @@
-Porjeto em JAVA 8, MYSQL, utilizando o Netbens 8.2;
-
-Segir passos abaixo.
-
-Criar o banco de dados, utililizando o script do Arquivo Trabalho Funcionando SQL.
+Criar o banco de dados do Arquivo Trabalho Funcionando SQL.
 
 Trocar o Usuario e senha do seu banco, juntamente com a porta, 
 
-Arquivo:
 src\br\com\sindicoonline\DAO\conexao.class
 
             conn = DriverManager.getConnection(
